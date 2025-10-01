6db4e94d24d7cd06c860960dad936868ce85ffd8
xavier wallis <wallisxdevelopment@gmail.com>
2025-10-01 12:57:14 -0400

    database auth and docker compose gfys

--- src/server/Database.cs
+++ src/server/Database.cs
@@ -3,8 +3,18 @@ namespace BarterDatabase {
   
   // users table schema
   public class User {
-    public required string ID { get; set; }
+    public required Guid ID { get; set; } = Guid.NewGuid();
     public required string Email { get; set; }
+    public string? Name { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    // Username/Password Authentication
+    public string? PasswordSalt { get; set; }                // Null if not using password auth
+    public string? PasswordHash { get; set; }
+
+    // OAuth Authentication
+    public string? OAuthProvider { get; set; }               // e.g. "google", "microsoft", "facebook"
   }
 
   public class Database : DbContext {
