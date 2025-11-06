c979d5cc5a2240386568e6e58ebeec402dd44c27
xavier wallis <wallisxdevelopment@gmail.com>
2025-11-06 17:46:37 -0500

    added seaweed

--- src/server/Contracts/ItemRequests.cs
+++ src/server/Contracts/ItemRequests.cs
@@ -1,4 +1,3 @@
 using Library.Storage;
 public record CreateItemRequest( Guid OwnerID, string Name, string? Description, Category Category, Guid[]? Images = null );
 public record UpdateItemRequest( Guid ID, string Name, string? Description, Category Category, Guid[]? Images = null );
-public record DeleteItemRequest( Guid ID );
